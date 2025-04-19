eb524dd7f6f6b33c715462ed7e6afe257c4c6e66
Migovich <migovichdev@gmail.com>
2025-04-19 15:12:31 +0700

    added onTapGesture to toggle reminder isCompleted state

--- MakeItSo/ContentView.swift
+++ MakeItSo/ContentView.swift
@@ -12,11 +12,14 @@ struct ContentView: View {
     @State private var reminders = Reminder.samples
     
     var body: some View {
-        List(reminders) { reminder in
+        List($reminders) { $reminder in
             HStack {
                 Image(systemName: reminder.isCompleted ? "checkmark.circle.fill" : "circle")
                     .imageScale(.large)
                     .foregroundStyle(.tint)
+                    .onTapGesture {
+                        reminder.isCompleted.toggle()
+                    }
                 Text(reminder.title)
             }
         }
