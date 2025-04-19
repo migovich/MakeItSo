e86d9bc8877555583d77c39a880b33479f69c797
Migovich <migovichdev@gmail.com>
2025-04-19 14:37:57 +0700

    Initial Commit

---
+++ MakeItSo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MakeItSo
+//
+//  Created by Migovich on 19.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MakeItSo/MakeItSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeItSoApp.swift
+//  MakeItSo
+//
+//  Created by Migovich on 19.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MakeItSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
