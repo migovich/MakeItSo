63e4f6042b0a8e1662a98140d689f6bf7851a8c1
Migovich <migovichdev@gmail.com>
2025-05-03 12:34:37 +0800

    Present add reminder dialog

--- MakeItSo/AddReminderView.swift
+++ MakeItSo/AddReminderView.swift
@@ -10,6 +10,16 @@ import SwiftUI
 struct AddReminderView: View {
     @State private var reminder = Reminder(title: "")
     
+    @Environment(\.dismiss)
+    private var dismiss
+    
+    var onCommit: (_ reminder: Reminder) -> Void
+    
+    private func commit() {
+        onCommit(reminder)
+        dismiss()
+    }
+    
     var body: some View {
         NavigationStack {
             Form {
@@ -17,8 +27,8 @@ struct AddReminderView: View {
             }
             .toolbar {
                 ToolbarItem(placement: .confirmationAction) {
-                    Button("Add") {
-                        print("Add button pressed")
+                    Button(action: commit) {
+                        Text("Add")
                     }
                 }
             }
@@ -27,5 +37,7 @@ struct AddReminderView: View {
 }
 
 #Preview {
-    AddReminderView()
+    AddReminderView { reminder in
+        print("You added a new reminder: \(reminder.title)")
+    }
 }
--- MakeItSo/ContentView.swift
+++ MakeItSo/ContentView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var reminders = Reminder.samples
+    @State private var isAddReminderPresented = false
+    
+    private func presendAddReminderView() {
+        isAddReminderPresented.toggle()
+    }
     
     var body: some View {
         List($reminders) { $reminder in
@@ -23,9 +28,28 @@ struct ContentView: View {
                 Text(reminder.title)
             }
         }
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button(action: presendAddReminderView) {
+                    HStack {
+                        Image(systemName: "plus.circle.fill")
+                        Text("New Reminder")
+                    }
+                }
+                Spacer()
+            }
+        }
+        .sheet(isPresented: $isAddReminderPresented) {
+            AddReminderView { reminder in
+                reminders.append(reminder)
+            }
+        }
     }
 }
 
 #Preview {
-    ContentView()
+    NavigationStack {
+        ContentView()
+            .navigationTitle("Reminders")
+    }
 }
